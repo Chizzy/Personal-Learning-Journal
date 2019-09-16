cf3778dc7fba7a57daebc553cdacb018484c21b7
Chizzy <chizoba.princess@gmail.com>
2019-09-16 13:35:51 -0400

    pulled out the header and footer

---
+++ inc/footer.php
@@ -0,0 +1,9 @@
+</div> <!-- end container -->
+        </section>
+        <footer>
+            <div>
+                &copy; <?php echo date('Y'); ?> MyJournal
+            </div>
+        </footer>
+    </body>
+</html>
